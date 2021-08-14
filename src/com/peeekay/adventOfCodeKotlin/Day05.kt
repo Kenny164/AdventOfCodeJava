11ca39be0c7351a49524cfc62ba3f19ef87fc00b
Kenny164 <peter.kinney@gmail.com>
2021-08-14 17:14:40 +0100

    kt day05 part 2

--- src/com/peeekay/adventOfCodeKotlin/Day05.kt
+++ src/com/peeekay/adventOfCodeKotlin/Day05.kt
@@ -1,26 +1,32 @@
 package com.peeekay.adventOfCodeKotlin
 
 class Day05 : AOCPuzzle(5) {
-    val rawInput = resourceAsList()
+    private val rawInput = resourceAsList()
 
     private fun strToSeatId(boardingNum: String): Int {
-        val result = boardingNum.map {
+        return boardingNum
+            .map {
             when (it) {
                 'F', 'L' -> '0'
                 'B', 'R' -> '1'
                 else -> it
-            }
-        }.joinToString("").toInt(2)
-        return result
+            }}
+            .joinToString("").toInt(2)
     }
+
+    private val seats = rawInput.map { strToSeatId(it) }
 
     override fun partOne(): Any? {
         //return strToSeatId("BFFFBBFRRR")
-        return rawInput.map { strToSeatId(it) }.maxOrNull()
+        return seats.maxOrNull()
     }
 
     override fun partTwo(): Any? {
-        TODO("Not yet implemented")
+        return seats
+            .sorted()
+            .zipWithNext()
+            .filter {it.first + 1 != it.second}
+            .first().first + 1
     }
 
 
