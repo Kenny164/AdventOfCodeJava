b27a8df3e2b6ac695c310ac4ff601ff60a7eb835
Kenny164 <peter.kinney@gmail.com>
2022-02-06 15:34:17 +0000

    completed 2021 day08 p2

--- src/com/peeekay/aoc2021/kotlin/Day08.kt
+++ src/com/peeekay/aoc2021/kotlin/Day08.kt
@@ -2,16 +2,33 @@ package com.peeekay.aoc2021.kotlin
 
 class Day08 : AOCPuzzle(8) {
     private val inputs = resourceAsList()
-        .map { it.split(" ").filterNot { it == "|" }.map{it.toSet()} }
+        .map { line -> line.split(" ").filterNot { it == "|" }.map{it.toSet()} }
 
     override fun partOne(): Any? {
         val simpleDigits = setOf(2, 3, 4, 7)
-        return inputs
-            .sumOf { it.takeLast(4).count {it.size in simpleDigits }}
+        return inputs.sumOf { line ->
+            line.takeLast(4).count { it.size in simpleDigits }}
     }
 
     override fun partTwo(): Any? {
-        TODO("Not yet implemented")
+        return inputs.sumOf{ line ->
+            line.groupBy() { it.size }
+                .let {
+                    val number = Array(10) { setOf<Char>() }
+                    number[1] = it[2]!!.first()
+                    number[4] = it[4]!!.first()
+                    number[7] = it[3]!!.first()
+                    number[8] = it[7]!!.first()
+                    number[6] = it[6]!!.first { number[8] == number[1] + it }
+                    number[5] = it[5]!!.first { number[8] != number[6] + it }
+                    number[2] = it[5]!!.first { number[8] == number[5] + it }
+                    number[9] = it[6]!!.first { number[8] != number[4] + it }
+                    number[0] = it[6]!!.first { number[8] == number[5] + it }
+                    number[3] = it[5]!!.first { it !in number }
+                    line.takeLast(4).map { number.indexOf(it) }.joinToString("").toInt()
+                }
+        }
+
     }
 
 }
