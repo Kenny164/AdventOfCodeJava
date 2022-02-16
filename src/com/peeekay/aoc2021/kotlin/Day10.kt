59f3f83695a76861868ce631cdae45289168d7bd
Kenny164 <peter.kinney@gmail.com>
2022-02-16 23:22:14 +0000

    completed 2021 day10 p2

--- src/com/peeekay/aoc2021/kotlin/Day10.kt
+++ src/com/peeekay/aoc2021/kotlin/Day10.kt
@@ -12,11 +12,19 @@ class Day10 : AOCPuzzle(10) {
         '>' to 25137
     )
 
+    private val completionPoints = mapOf(
+        ')' to 1,
+        ']' to 2,
+        '}' to 3,
+        '>' to 4
+    )
+
     override fun partOne(): Any? {
         return input.map { it.firstBadCharOrNull() }
             .groupingBy { it }.eachCount()
             .map { (char, count) ->
-            errorPoints.getOrDefault(char, 0).toLong() * count }.sum()
+                errorPoints.getOrDefault(char, 0).toLong() * count }
+            .sum()
     }
 
     private fun String.firstBadCharOrNull(): Char? {
@@ -28,8 +36,25 @@ class Day10 : AOCPuzzle(10) {
         return null
     }
 
-    override fun partTwo(): Any? {
-        TODO("Not yet implemented")
+    private fun String.incompleteStackOrNull(): ArrayDeque<Char>? {
+        val stack = ArrayDeque<Char>()
+        this.forEach { c ->
+            if (c in openToCloseMap.keys) stack.addLast(c)
+            else if (stack.isNotEmpty() && openToCloseMap[stack.removeLast()] != c) return null
+        }
+        return stack
     }
-
+    override fun partTwo(): Any? {
+        return input.mapNotNull { it.incompleteStackOrNull() }
+            .map { stack ->
+                stack.reversed()
+                    .mapNotNull { c ->
+                        completionPoints[openToCloseMap[c]]
+                    }
+                    .fold(0L) { a, b ->
+                        (a * 5) + b }
+                    }
+            .sorted()
+            .let { it[it.lastIndex/2] }
+    }
 }
