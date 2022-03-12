1b4d80b7a03b5037632389a30842170de22d0b06
Kenny164 <peter.kinney@gmail.com>
2022-03-12 17:38:12 +0000

    completed 2021 day12 p2

--- src/com/peeekay/aoc2021/kotlin/Day12.kt
+++ src/com/peeekay/aoc2021/kotlin/Day12.kt
@@ -9,9 +9,9 @@ class Day12 : AOCPuzzle(12) {
                    l.last() to l.first())
         }.groupBy ({it.first}, {it.second})
 
-    private fun Map<String, List<String>>.steps(): MutableSet<List<String>> {
-        var caveQ = mutableListOf(listOf("start"))
-        var allPaths = mutableSetOf<List<String>>()
+    private fun Map<String, List<String>>.steps(ruleSet:(String, List<String>)-> Boolean): MutableSet<List<String>> {
+        val caveQ = mutableListOf(listOf("start"))
+        val allPaths = mutableSetOf<List<String>>()
 
         while (caveQ.isNotEmpty()){
             val path = caveQ.removeFirst()
@@ -22,8 +22,8 @@ class Day12 : AOCPuzzle(12) {
                 continue
             }
 
-            cave.getOrDefault(curParent, listOf()).forEach { c ->
-                if (c.all { !it.isLowerCase() } || c !in path)
+            this.getOrDefault(curParent, listOf()).forEach { c ->
+                if (ruleSet(c, path))
                     caveQ.add(path + c)
             }
         }
@@ -31,12 +31,26 @@ class Day12 : AOCPuzzle(12) {
         return allPaths
     }
 
+    private fun partOneRules(c: String, path: List<String>) =
+        c.all { !it.isLowerCase() } || c !in path
+
+    private fun partTwoRules(c: String, path: List<String>) =
+        when {
+            c == "start" -> false
+            c.all { !it.isLowerCase() } -> true
+            c !in path -> true
+            else -> path
+                .filterNot { w -> w.all { !it.isLowerCase() }}
+                .groupBy { it }
+                .none { it.value.size == 2 }
+        }
+
     override fun partOne(): Any? {
-        return cave.steps().size
+        return cave.steps(::partOneRules).size
     }
 
     override fun partTwo(): Any? {
-        TODO("Not yet implemented")
+        return cave.steps(::partTwoRules).size
     }
 
 }
