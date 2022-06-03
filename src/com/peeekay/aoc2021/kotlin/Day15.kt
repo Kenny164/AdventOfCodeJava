a08dfe9a88581b2f572be86cc0c1ee8d927b17e0
Kenny164 <peter.kinney@gmail.com>
2022-06-03 19:36:59 +0100

    day 15 cleanup part one, finish part 2

--- src/com/peeekay/aoc2021/kotlin/Day15.kt
+++ src/com/peeekay/aoc2021/kotlin/Day15.kt
@@ -10,30 +10,44 @@ class Day15 : AOCPuzzle(15){
                 .map{it.digitToInt()}
         }
 
-    override fun partOne(): Any? {
-        val pq = PriorityQueue<Triple<Int, Int, Int>> {a, b ->
+    private fun calcRisk(multiplier: Int): Int {
+        val pq = PriorityQueue<Triple<Int, Int, Int>> { a, b ->
             a.first - b.first
         }
-        val dest = Pair(cave.size-1,cave[0].size-1)
-        pq.add(Triple(0,0,0))
-        var visited = mutableSetOf<Pair<Int,Int>>()
+        val maxX = cave[0].size * multiplier
+        val maxY = cave.size * multiplier
+        val dest = Pair(maxY - 1, maxX - 1)
+        pq.add(Triple(0, 0, 0))
+        var visited = mutableSetOf<Pair<Int, Int>>()
         while (pq.isNotEmpty()) {
             val (d, x, y) = pq.poll()
             if (Pair(x, y) == dest)
                 return d
             listOf(x to y + 1, x to y - 1, x + 1 to y, x - 1 to y)
                 .forEach { (X, Y) ->
-                    if (X in cave.indices && Y in cave[0].indices && Pair(X,Y) !in visited){
-                        visited.add(Pair(X,Y))
-                        pq.offer(Triple(cave[X][Y] + d, X, Y))
+                    if (X in 0 until maxX &&
+                        Y in 0 until maxY &&
+                        Pair(X, Y) !in visited) {
+                        val dx = X / cave[0].size
+                        val dy = Y / cave.size
+                        //println(Pair(X, Y))
+                        val originalRisk = cave[Y % cave.size][X % cave[0].size]
+                        val newRisk = (originalRisk + dx + dy)
+                        val newerRisk = newRisk.takeIf { it < 10 } ?: (newRisk - 9)
+                        visited.add(Pair(X, Y))
+
+                        pq.offer(Triple(newerRisk + d, X, Y))
                     }
                 }
         }
-
         return 0 // will never be reached
     }
 
+    override fun partOne(): Any? {
+        return calcRisk(1)
+    }
+
     override fun partTwo(): Any? {
-        TODO("Not yet implemented")
+        return calcRisk(5)
     }
 }
