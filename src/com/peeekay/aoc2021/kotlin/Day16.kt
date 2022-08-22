a8d08b711177ea850cbbede5e4ca15c2c1309b62
Kenny164 <peter.kinney@gmail.com>
2022-08-23 00:53:16 +0100

    completed 2021 day16 p1

--- src/com/peeekay/aoc2021/kotlin/Day16.kt
+++ src/com/peeekay/aoc2021/kotlin/Day16.kt
@@ -1,20 +1,54 @@
 package com.peeekay.aoc2021.kotlin
 
 class Day16 : AOCPuzzle(16){
-    private val rawInput = resourceAsString_Test()
-    private val cave = rawInput
-        .map { it.digitToInt(16).toString(2).padStart(4,'0') }
-        .flatMap { it.toList() }
-        .iterator()
+    private val rawInput = resourceAsString()
+    private val cave = inputToBinIterator(rawInput)
+    var versionSum = 0
+
+    fun inputToBinIterator(inp: String) = inp
+            .map { it.digitToInt(16).toString(2).padStart(4, '0') }
+            .flatMap { it.toList() }
+            .iterator()
 
     private fun <T> Iterator<T>.takeNext(i: Int): String {
         return (1..i).map { this.next() }.joinToString("")
     }
 
+    fun parsePacketsRecurse(inp: Iterator<Char>): Long {
+        val version = inp.takeNext(3).toInt(2)
+        versionSum += version
+        val packetType = inp.takeNext(3).toInt(2)
+
+        if (packetType == 4) {
+            val payload = StringBuilder()
+            while (true) {
+                val pendingDataBit = inp.takeNext(1) == "1"
+                payload.append(inp.takeNext(4))
+                if (!pendingDataBit) break
+            }
+            return payload.toString().toLong(2)
+        }
+
+        val subPackets = mutableListOf<Long>()
+
+        val payLoadLengthBit = inp.takeNext(1) // The reason why packetType 4 logic is above
+        if (payLoadLengthBit == "0") {
+            val subPacketLength = inp.takeNext(15).toInt(2)
+            val payload = inp.takeNext(subPacketLength).iterator()
+            while (payload.hasNext()) {
+                subPackets.add(parsePacketsRecurse(payload))
+            }
+        } else {
+            val subPacketCount = inp.takeNext(11).toInt(2)
+            (1..subPacketCount).map { subPackets.add(parsePacketsRecurse(inp))}
+        }
+
+        return packetType.toLong()
+    }
+
     override fun partOne(): Any? {
-        val version = cave.takeNext(3).toInt(2)
-        val packetType = cave.takeNext(3).toInt(2)
-        return packetType
+        parsePacketsRecurse(cave)
+        return versionSum
     }
 
     override fun partTwo(): Any? {
