c19c20709173f63be8da4d255209639ca31f48b6
Kenny164 <peter.kinney@gmail.com>
2022-12-04 17:20:57 +0000

    day01 cleanup

--- src/com/peeekay/aoc2022/kotlin/Day01.kt
+++ src/com/peeekay/aoc2022/kotlin/Day01.kt
@@ -1,17 +1,17 @@
 package com.peeekay.aoc2022.kotlin
 
-class Day01(val isTest: Boolean = false) : AOCPuzzle(1, isTest){
-    private val inp: List<List<Int>> by lazy {
-        resourceAsSplitText("\r\n\r\n")
-            .map { elf_group ->
-                elf_group.lines().map { calorie ->
-                    calorie.toInt() } } }
+class Day01(val isTest: Boolean = false) : AOCPuzzle(1, isTest) {
+    private val inp: List<List<Int>> = resourceAsSplitText("\r\n\r\n").map { elfGroup ->
+        elfGroup.lines().map { calories ->
+            calories.toInt()
+        }
+    }
 
-    override fun partOne(): Any? {
+    override fun partOne(): Any {
         return inp.maxOf { it.sum() }
     }
 
-    override fun partTwo(): Any? {
+    override fun partTwo(): Any {
         return inp.map { it.sum() }.sortedDescending().take(3).sum()
     }
 
