1c684c5ec3491aec91e2ba54b7002d0462f8df62
Kenny164 <peter.kinney@gmail.com>
2022-12-07 19:52:28 +0000

    day06 fixer upper

--- src/com/peeekay/aoc2022/kotlin/Day06.kt
+++ src/com/peeekay/aoc2022/kotlin/Day06.kt
@@ -3,11 +3,14 @@ package com.peeekay.aoc2022.kotlin
 class Day06(val isTest: Boolean = false) : AOCPuzzle(6, isTest) {
     private val inp = resourceAsText()
 
-    override fun partOne(): Any? {
-        return inp.windowedSequence(4, 1).withIndex().first { it.value.toSet().size == 4 }.let { it.index + 4 }
-    }
+    private fun String.findUniqueWindow(windowSize: Int) =
+        this.windowedSequence(windowSize, 1)
+            .withIndex()
+            .first { it.value.toSet().size == windowSize }
+            .let { it.index + windowSize }
 
-    override fun partTwo(): Any? {
-        return inp.windowedSequence(14, 1).withIndex().first { it.value.toSet().size == 14 }.let { it.index + 14 }
-    }
+    override fun partOne(): Any? = inp.findUniqueWindow(4)
+
+    override fun partTwo(): Any? = inp.findUniqueWindow(14)
+
 }
