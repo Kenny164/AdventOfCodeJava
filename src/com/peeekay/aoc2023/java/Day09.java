d06eea39138db90d834a5da1440462d0d7ef9a75
Kenny164 <peter.kinney@gmail.com>
2024-06-26 23:05:06 +0100

    2023 day09 p1 (Java)

---
+++ src/com/peeekay/aoc2023/java/Day09.java
@@ -0,0 +1,50 @@
+package com.peeekay.aoc2023.java;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Day09 extends AOCPuzzle {
+    Object _part1, _part2;
+
+    public Day09(boolean isTest) {
+        super(9, isTest);
+    }
+
+    @Override
+    public Object part1() {
+        return _part1;
+    }
+
+    @Override
+    public Object part2() {
+        return _part2;
+    }
+
+    void solve() {
+        solve(this.resourceAsList());
+    }
+
+    void solve(List<String> inp) {
+        var histories = inp.stream()
+                .map(line -> Arrays.stream(line.split(" "))
+                        .map(Integer::parseInt).toList());
+
+        _part1 = histories
+                .mapToInt(hist -> Stream.iterate(hist,
+                                prediction -> !prediction.stream().allMatch(v -> v == 0),
+                                prediction -> IntStream.range(1, prediction.size())
+                                        .mapToObj(i -> prediction.get(i) - prediction.get(i - 1))
+                                        .toList())
+                        .mapToInt(prediction -> prediction.get(prediction.size() - 1))
+                        .sum())
+                .sum();
+
+        _part2 = 0L;
+
+
+    }
+
+
+}
---
+++ test/com/peeekay/aoc2023/java/Day09Test.java
@@ -0,0 +1,24 @@
+package com.peeekay.aoc2023.java;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Day09Test {
+    final AOCPuzzle day = new Day09(true);
+
+    @Test
+    void part1() {
+        day.solve();
+        assertEquals(114, day.part1());
+    }
+
+    @Test
+    void part2() {
+        day.solve();
+        assertEquals(0L, day.part2());
+    }
+
+}
