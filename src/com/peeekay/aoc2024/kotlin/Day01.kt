73c78b612656f5e1b2d31bc6ffdd6f528308a390
Kenny164 <peter.kinney@gmail.com>
2024-12-01 17:02:30 +0000

    2024 day1 p1 + p2 (refactored to be shorter)

--- src/com/peeekay/aoc2024/kotlin/Day01.kt
+++ src/com/peeekay/aoc2024/kotlin/Day01.kt
@@ -3,28 +3,22 @@ package com.peeekay.aoc2024.kotlin
 import kotlin.math.abs
 
 class Day01(val isTest: Boolean = false) : AOCPuzzle(1, isTest) {
-    private val pInput = splitStringsToLists(resourceAsList(), 2)
-
-    private fun splitStringsToLists(stringList: List<String>, numLists: Int): List<List<Int>> {
-        val outputLists = List(numLists) { mutableListOf<Int>() }
-        for (str in stringList) {
-            val parts = str.trim().split("\\s+".toRegex())
-                for (i in 0 until numLists) {
-                    outputLists[i].add(parts[i].toInt())
-            }
-        }
-        return outputLists
+    private val pInput = resourceAsList().fold(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { acc, str ->
+        val (first, second) = str.trim().split("\\s+".toRegex()).map { it.toInt() }
+        acc.first.add(first)
+        acc.second.add(second)
+        acc
     }
 
     override fun partOne(): Any {
-        return pInput[0].sorted()
-            .zip(pInput[1].sorted())
+        return pInput.first.sorted()
+            .zip(pInput.second.sorted())
             .sumOf { abs(it.second - it.first) }
     }
 
     override fun partTwo(): Any {
-        val similarities = pInput[1].groupingBy { it }.eachCount()
-        return pInput[0].sumOf { it * similarities.getOrDefault(it, 0) }
+        val similarities = pInput.second.groupingBy { it }.eachCount()
+        return pInput.first.sumOf { it * similarities.getOrDefault(it, 0) }
     }
 
 }
