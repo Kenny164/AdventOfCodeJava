01554787795e00641287cbfe8d1a09c442e8d80e
Kenny164 <peter.kinney@gmail.com>
2025-04-12 21:46:12 +0100

    2024 day07 part2 (java)

--- aoc2024/java/src/main/java/com/peeekay/aoc2024/java/Day07.java
+++ aoc2024/java/src/main/java/com/peeekay/aoc2024/java/Day07.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Day07 extends AOCPuzzle {
     List<SingleEquation> inp;
+    StringBuilder SB = new StringBuilder();
 
     record SingleEquation(long testVal, List<Long> numbers) {
         static SingleEquation from(String line) {
@@ -53,8 +54,42 @@ public class Day07 extends AOCPuzzle {
         }
     }
 
+    boolean canCalibrate2(SingleEquation singleEquation, long acc, List<Long> remaining) {
+        if (remaining.isEmpty()) {
+            return acc == singleEquation.testVal;
+        }
+        else if (acc > singleEquation.testVal) {
+            return false;
+        }
+        else {
+            long number = remaining.getFirst();
+            if (canCalibrate2(singleEquation, acc + number, remaining.subList(1, remaining.size()))) {
+                return true;
+            }
+            else if (canCalibrate2(singleEquation, acc * number, remaining.subList(1, remaining.size()))) {
+                return true;
+            }
+            else if (canCalibrate2(singleEquation, concatOp(acc, number), remaining.subList(1, remaining.size()))) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    private long concatOp (long a, long b) {
+        SB.setLength(0);
+        SB.append(a).append(b);
+        return Long.parseLong(SB.toString());
+    }
+
     @Override
     public Object part2() {
-        return 0;
+        return inp.stream()
+                .filter(se -> canCalibrate2(se, 0L, se.numbers))
+                .map(e -> e.testVal)
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 }
