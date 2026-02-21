cb45a49ac66e73d7554762180877e3684a2b4799
Kenny164 <peter.kinney@gmail.com>
2026-02-21 17:15:46 +0000

    2025 day06 p2 (java)

--- aoc2025/java/src/main/java/com/peeekay/aoc2025/java/Day06.java
+++ aoc2025/java/src/main/java/com/peeekay/aoc2025/java/Day06.java
@@ -6,12 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day06 extends AOCPuzzle {
-    List<Problem> problems = new ArrayList<>();
+    private final List<String> inp = resourceAsList();
 
     public Day06(boolean isTest) {
         super(2025, 6, isTest);
-        List<String> inp = resourceAsList();
+    }
+    
+    private List<Problem> getProblems(List<String> inp) {
+        return getProblems(inp, false);
+    }
 
+    private List<Problem> getProblems(List<String> inp, boolean isPart2) {
+        List<Problem> problems = new ArrayList<>();
         String operationsLine = inp.getLast();
         Character currentOp = null;
         int lastOpIndex = 0;
@@ -19,18 +25,28 @@ public class Day06 extends AOCPuzzle {
             char cur = operationsLine.charAt(i);
             if (cur != ' ') {
                 if (currentOp != null) {
-                    problems.add(new Problem(currentOp, getProbData(lastOpIndex, i - 1 - lastOpIndex, inp)));
+                    if (isPart2) {
+                        problems.add(new Problem(currentOp, getProbData2(lastOpIndex, i - 1 - lastOpIndex, inp)));
+                    } else {
+                        problems.add(new Problem(currentOp, getProbData(lastOpIndex, i - 1 - lastOpIndex, inp)));
+                    }
                 }
                 currentOp = cur;
                 lastOpIndex = i;
             }
         }
         if (currentOp != null) {
-            problems.add(new Problem(currentOp, getProbData(lastOpIndex, operationsLine.length() - lastOpIndex, inp)));
+            if (isPart2) {
+                problems.add(new Problem(currentOp, getProbData2(lastOpIndex, operationsLine.length() - lastOpIndex, inp)));                
+            } else {
+                problems.add(new Problem(currentOp, getProbData(lastOpIndex, operationsLine.length() - lastOpIndex, inp)));
+            }
         }
+        return problems;
     }
 
-    record Problem(char operation, List<Long> data) {}
+    record Problem(char operation, List<Long> data) {
+    }
 
     private List<Long> getProbData(int startIdx, int width, List<String> inp) {
         StringBuilder sb = new StringBuilder();
@@ -49,24 +65,46 @@ public class Day06 extends AOCPuzzle {
             }
         }
         return result;
+    }    
+    
+    private List<Long> getProbData2(int startIdx, int width, List<String> inp) {
+        StringBuilder sb = new StringBuilder();
+        List<Long> result = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            sb.setLength(0);
+            for (int j = 0; j < inp.size() - 1; j++) {
+                char cur = inp.get(j).charAt(startIdx + i);
+                if (cur == ' ') {
+                    continue;
+                }
+                sb.append(cur);
+            }
+            if (!sb.isEmpty()) {
+                result.add(Long.parseLong(sb.toString()));
+            }
+        }
+        return result;
+    }
+
+    private Long applyOperation(Problem p) {
+        if (p.operation == '+') {
+            return p.data.stream().reduce(0L, Long::sum);
+        } else {
+            return p.data.stream().reduce(1L, (a, b) -> a * b);
+        }
     }
 
     @Override
     public Object part1() {
-        return problems.stream()
-                .peek(System.out::println)
-                .map(p -> { 
-                    if (p.operation == '+') {
-                        return p.data.stream().reduce(0L, Long::sum);
-                    } else {
-                        return p.data.stream().reduce(1L, (a, b) -> a * b);
-                    }
-                })
+        return getProblems(inp).stream()
+                .map(this::applyOperation)
                 .reduce(0L, Long::sum);
     }
 
     @Override
     public Object part2() {
-        return 0;
+        return getProblems(inp, true).stream()
+                .map(this::applyOperation)
+                .reduce(0L, Long::sum);
     }
 }
