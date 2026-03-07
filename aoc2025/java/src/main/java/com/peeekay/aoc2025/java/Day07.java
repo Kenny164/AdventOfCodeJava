88df69f62c2e25a2235446ce73e618bd84f85ea3
Kenny164 <peter.kinney@gmail.com>
2026-03-07 18:58:40 +0000

    2025 day07 p2 (java)

--- aoc2025/java/src/main/java/com/peeekay/aoc2025/java/Day07.java
+++ aoc2025/java/src/main/java/com/peeekay/aoc2025/java/Day07.java
@@ -2,42 +2,56 @@ package com.peeekay.aoc2025.java;
 
 import com.peeekay.aocCommon.AOCPuzzle;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Day07 extends AOCPuzzle {
     List<String> inp = resourceAsList();
+    long splitCount = 0L;
+    long[] beams;
 
     public Day07(boolean isTest) {
         super(2025, 7, isTest);
+        
+        solve();
+    }
+    
+    private void solve() {
+        int start = inp.getFirst().indexOf('S');
+        beams = new long[inp.getFirst().length()];
+        beams[start] = 1;
+
+        for (int row = 1; row < inp.size(); row++) {
+            long[] nextBeams = new long[beams.length];
+            
+            for (int col = 0; col < inp.get(row).length(); col++) {
+                long currentBeamCount = beams[col];
+                if (currentBeamCount == 0) continue;
+
+                if (inp.get(row).charAt(col) == '^') {
+                    splitCount++;
+                    if (col > 0) {
+                        nextBeams[col - 1] += currentBeamCount;
+                    }
+                    if (col < inp.get(row).length() - 1) {
+                        nextBeams[col + 1] += currentBeamCount;
+                    }
+                } else {
+                    nextBeams[col] += currentBeamCount;
+                }
+            }
+            beams = nextBeams;
+        }  
     }
 
     @Override
     public Object part1() {
-        int start = inp.getFirst().indexOf('S');
-        boolean[] acc = new boolean[inp.getFirst().length()];
-        acc[start] = true;
-        long splitCount = 0L;
-        
-        for (int row = 1; row < inp.size(); row++) {
-            for (int col = 0; col < inp.get(row).length(); col++) {                
-                if (inp.get(row).charAt(col) == '^' && acc[col]) {
-                    splitCount++;
-                    //System.out.println("Split: " + splitCount + ", " +  row + ", " + col + " int[]: " + Arrays.toString(acc));
-                    acc[col] = false;
-                    if (col > 0) {
-                        acc[col - 1] = true;
-                    }
-                    if (col < inp.get(row).length() - 1) {
-                        acc[col + 1] = true;
-                    }
-                }
-            }
-        }
         return splitCount;
     }
 
     @Override
     public Object part2() {
-        return 0;
+        return Arrays.stream(beams)
+                .sum();
     }
 }
--- aoc2025/java/src/test/java/com/peeekay/aoc2025/java/Day07Test.java
+++ aoc2025/java/src/test/java/com/peeekay/aoc2025/java/Day07Test.java
@@ -14,6 +14,6 @@ public class Day07Test {
 
     @Test
     void part2() {
-        Assertions.assertEquals(0, day.part2());
+        Assertions.assertEquals(40L, day.part2());
     }
 }
